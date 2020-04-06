69c3eb4d11c3542b8b7f9176ef83099a51430fda
saramorrison <60265404+saramorrison@users.noreply.github.com>
2020-04-06 00:00:12 -0400

    Update NearestNeighborClassification.py

--- NearestNeighborClassification.py
+++ NearestNeighborClassification.py
@@ -4,6 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+from scipy.spatial import distance
 
 
 # FUNCTIONS
@@ -12,13 +13,28 @@ def openckdfile():
     return glucose, hemoglobin, classification
 
 
+def createTestCase():
+    newglucose = random.randint(1, 500)
+    newhemoglobin = random.randint(1, 20)
+    plt.plot([newhemoglobin], [newglucose], marker = "o", markersize = 6, color = "limegreen")
+    return newhemoglobin, newglucose
+    plt.show
 
-
+def calculateDistanceArray():
+    newglucose = random.randint(1, 500)
+    newhemoglobin = random.randint(1, 20)
+    a = (newhemoglobin, newglucose)
+    b = (hemoglobin, glucose)
+    dist = distance.euclidean(a, b)
+    return np.array(dist)
 
 # MAIN SCRIPT
 glucose, hemoglobin, classification = openckdfile()
 
+calculateDistanceArray()
+
 plt.figure()
+createTestCase()
 plt.plot(hemoglobin[classification==1],glucose[classification==1], "k.", label = "Class 1")
 plt.plot(hemoglobin[classification==0],glucose[classification==0], "r.", label = "Class 0")
 plt.xlabel("Hemoglobin")
