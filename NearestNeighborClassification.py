fcb86d96cc932376a7bd4c3a0a79ac1be07b0a21
saramorrison <60265404+saramorrison@users.noreply.github.com>
2020-04-07 10:30:53 -0400

    Update NearestNeighborClassification.py

--- NearestNeighborClassification.py
+++ NearestNeighborClassification.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 from scipy.spatial import distance
+import math
 
 
 # FUNCTIONS
@@ -12,26 +13,50 @@ def openckdfile():
     glucose, hemoglobin, classification = np.loadtxt('ckd.csv', delimiter=',', skiprows=1, unpack=True)
     return glucose, hemoglobin, classification
 
+def normalizeData(glucose, hemoglobin, classification):
+    glucose_normalized = []
+    hemoglobin_normalized = []
+    for g in glucose:
+        glucose_normalized.append((g / 70)/(490 - 70))
+    for h in hemoglobin:
+        hemoglobin_normalized.append((h - 3.1)/(17.8 - 3.1))
+    glucose_normalized = np.array(glucose_normalized)
+    hemoglobin_normalized = np.array(hemoglobin_normalized)
+    return glucose_normalized, hemoglobin_normalized, classification
+    
 
 def createTestCase():
     newglucose = random.randint(1, 500)
     newhemoglobin = random.randint(1, 20)
-    plt.plot([newhemoglobin], [newglucose], marker = "o", markersize = 6, color = "limegreen")
+#    plt.plot([newhemoglobin], [newglucose], marker = "o", markersize = 6, color = "limegreen")
+#    plt.show
     return newhemoglobin, newglucose
-    plt.show
+
+def Distance(newhemoglobin, hemoglobin, newglucose, glucose):
+    dist = math.sqrt(((newhemoglobin-hemoglobin)**2)+(newglucose-glucose)**2)
+    return dist
 
 def calculateDistanceArray():
-    newglucose = random.randint(1, 500)
-    newhemoglobin = random.randint(1, 20)
-    a = (newhemoglobin, newglucose)
-    b = (hemoglobin, glucose)
-    dist = distance.euclidean(a, b)
-    return np.array(dist)
+    n = 0
+    DistanceArray = []
+    for i in classification:
+        g = glucose[n]
+        h = hemoglobin[n]
+        DistanceArray.append(distance(newhemoglobin, h, newglucose, g))
+        n += 1
+    return DistanceArray
+
+
 
 # MAIN SCRIPT
+    
 glucose, hemoglobin, classification = openckdfile()
+glucose, hemoglobin, classification = normalizeData(glucose, hemoglobin, classification)
+newpoint = ()
+newglucose, newhemoglobin = createTestCase()
 
-calculateDistanceArray()
+
+
 
 plt.figure()
 createTestCase()
