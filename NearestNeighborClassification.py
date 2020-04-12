efac6c381eed3a47f0c40e7c20e3178fe3dcf08b
saramorrison <60265404+saramorrison@users.noreply.github.com>
2020-04-11 23:29:13 -0400

    Update NearestNeighborClassification.py

--- NearestNeighborClassification.py
+++ NearestNeighborClassification.py
@@ -17,19 +17,26 @@ def normalizeData(glucose, hemoglobin, classification):
     glucose_normalized = []
     hemoglobin_normalized = []
     for g in glucose:
-        glucose_normalized.append((g / 70)/(490 - 70))
+        glucose_normalized.append((g - 70)/(420))
     for h in hemoglobin:
-        hemoglobin_normalized.append((h - 3.1)/(17.8 - 3.1))
+        hemoglobin_normalized.append((h - 3.1)/(14.7))
     glucose_normalized = np.array(glucose_normalized)
     hemoglobin_normalized = np.array(hemoglobin_normalized)
     return glucose_normalized, hemoglobin_normalized, classification
-    
+
+def GraphData(glucose, hemoglobin, classification):
+    plt.figure()
+    createTestCase()
+    plt.plot(hemoglobin[classification==1],glucose[classification==1], "k.", label = "Class 1")
+    plt.plot(hemoglobin[classification==0],glucose[classification==0], "r.", label = "Class 0")
+    plt.xlabel("Hemoglobin")
+    plt.ylabel("Glucose")
+    plt.legend()
+    plt.show()    
 
 def createTestCase():
-    newglucose = random.randint(1, 500)
-    newhemoglobin = random.randint(1, 20)
-#    plt.plot([newhemoglobin], [newglucose], marker = "o", markersize = 6, color = "limegreen")
-#    plt.show
+    newglucose = random.uniform(0, 1)
+    newhemoglobin = random.uniform(0, 1)
     return newhemoglobin, newglucose
 
 def Distance(newhemoglobin, hemoglobin, newglucose, glucose):
@@ -42,9 +49,19 @@ def calculateDistanceArray():
     for i in classification:
         g = glucose[n]
         h = hemoglobin[n]
-        DistanceArray.append(distance(newhemoglobin, h, newglucose, g))
+        DistanceArray.append(Distance(newhemoglobin, h, newglucose, g))
         n += 1
     return DistanceArray
+
+def graphTestCase(newglucose, newhemoglobin, glucose, hemoglobin, classification):
+    plt.figure()
+    plt.plot(hemoglobin[classification==1],glucose[classification==1], "k.", label = "Class 1")
+    plt.plot(hemoglobin[classification==0],glucose[classification==0], "r.", label = "Class 0")
+    plt.xlabel("Hemoglobin")
+    plt.ylabel("Glucose")
+    plt.legend() 
+    plt.plot([newhemoglobin], [newglucose], marker = "o", markersize = 6, color = "limegreen")
+    plt.show
 
 
 
@@ -54,15 +71,5 @@ glucose, hemoglobin, classification = openckdfile()
 glucose, hemoglobin, classification = normalizeData(glucose, hemoglobin, classification)
 newpoint = ()
 newglucose, newhemoglobin = createTestCase()
+graphTestCase(newglucose, newhemoglobin, glucose, hemoglobin, classification)
 
-
-
-
-plt.figure()
-createTestCase()
-plt.plot(hemoglobin[classification==1],glucose[classification==1], "k.", label = "Class 1")
-plt.plot(hemoglobin[classification==0],glucose[classification==0], "r.", label = "Class 0")
-plt.xlabel("Hemoglobin")
-plt.ylabel("Glucose")
-plt.legend()
-plt.show()
