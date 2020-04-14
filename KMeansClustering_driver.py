8fed59c32232027f63f48b4b37c660ecb9860ca5
saramorrison <60265404+saramorrison@users.noreply.github.com>
2020-04-14 14:51:05 -0400

    final commit

--- KMeansClustering_driver.py
+++ KMeansClustering_driver.py
@@ -1,11 +1,20 @@
-#Please place your FUNCTION code for step 4 here.
+#File Name: KMeansClustering_driver
+
+#This file is the driver file. It imports the functions from KMeansClustering_functions and runs them. It will display the final graph and the accuracy percentages when ran.
+
+#Name: Sara Morrison
+#Estimated time on this file: .25 hours
 
 import KMeansClustering_functions as kmc #Use kmc to call your functions
 
-glucose, hemoglobin, classification = kmc.openckdfile()
-glucose, hemoglobin, classification = kmc.normalizeData(glucose, hemoglobin, classification)
+glucose, hemoglobin, classification = kmc.openckdfile()  #opens file
+glucose, hemoglobin, classification = kmc.normalizeData(glucose, hemoglobin, classification)  #assigns glucose, hemoglobin, and classificaiton to the normalized data
 
-k = 2
+k = 2  #assigns a k value, can be changed if desired
 
-assigned_centroids, newcentroid, glucose, hemoglobin, classification = kmc.Iterate(2, 100)
-kmc.graphingKMeans(glucose, hemoglobin, assigned_centroids, newcentroid)
+number_of_runs = 100  #number of times that Iterate will be run, can be changed
+
+assignment, newcentroid, glucose, hemoglobin, classification = kmc.Iterate(k, number_of_runs)  #runs the Iterate function with desired k value and number of runs
+kmc.graphingKMeans(glucose, hemoglobin, assignment, newcentroid)  #runs the graphing function with the outputs of the Iterate function
+
+kmc.Accuracy(classification, assignment)  #runs the accuracy function with the outputs from the iterate function
